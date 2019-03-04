5691a05dd192db6f5c6e431c0dcdbf526e13aef5
Elizabeth Kosowski <elizabethkosowski@gmail.com>
2019-03-04 14:55:52 -0500

    seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+shaggy = Owner.create(first_name: "Shaggy", last_name: "Rogers", phone_num: "555-867-5309", email: "shaggy@gmail.com")
+charlie = Owner.create(first_name: "Charlie", last_name: "Muntz", phone_num: "", email: "")
+
+scooby_doo = Dog.create(name: "Scooby Doo", breed: "Great Dane", birthday: Date.new(1969,1,1), img_url: "", owner_id: shaggy.id)
+dug = Dog.create(name: "Dug", breed: "Golden Retriever", birthday: Date.new(2009,5,29), img_url: "", owner_id: charlie.id)
+
+dr_kim = Vet.create(first_name: "Jenny", last_name: "Kim", address: "11 Broadway", phone_num: "123-456-7890", email: "drkim@gmail.com")
+dr_phillips = Vet.create(first_name: "Jack", last_name: "Phillips", address: "12 Broadway", phone_num: "123-456-7899", email: "drphillips@gmail.com")
+
+Patient.create(vet_id: dr_kim.id, dog_id: scooby_doo.id)
+Patient.create(vet_id: dr_phillips.id, dog_id: dug.id)
+
+instruction1 = Instruction.create(header: "Feeding Instructions", content: "Eats 10 times a day. Gets angry if you forget to feed him.", dog_id: scooby_doo.id)
+instruction2 = Instruction.create(header: "Walking Instructions", content: "Walk him near crime spots.", dog_id: scooby_doo.id)
+instruction3 = Instruction.create(header: "Feeding Instructions", content: "Loves to eat bacon.", dog_id: dug.id)
+instruction4 = Instruction.create(header: "Walking Instructions", content: "Plays only with his tennis ball while walking.", dog_id: dug.id)
