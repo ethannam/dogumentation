adac940dbe7b290e050b23c43aa4b6f9a8d545d9
Elizabeth Kosowski <elizabethkosowski@gmail.com>
2019-03-06 14:06:23 -0500

    birthday validation

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -9,4 +9,12 @@ class Dog < ApplicationRecord
   validates :breed, presence: true, length: { maximum: 30 }
   validates :user_id, presence: true
   validates :username, uniqueness: true, length: { maximum: 30 }
+  validate :birthday_cannot_be_in_future
+
+  def birthday_cannot_be_in_future
+    if self.birthday >= Time.now
+      errors.add(:birthday, "is in the future so the dog doesn't exist yet!")
+    end
+  end
+
 end
